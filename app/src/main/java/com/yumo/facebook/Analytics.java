d85f322eab28a24c07360c9db82278c358373953
yumodev <yumotingyu@gmail.com>
2019-07-22 06:59:45 +0800

    update

--- app/src/main/java/com/yumo/facebook/Analytics.java
+++ app/src/main/java/com/yumo/facebook/Analytics.java
@@ -12,6 +12,6 @@ public class Analytics {
     }
 
     public void logSentFriendRequestEvent (String event) {
-        logger.logEvent("sentFriendRequest");
+        logger.logEvent(event);
     }
 }
